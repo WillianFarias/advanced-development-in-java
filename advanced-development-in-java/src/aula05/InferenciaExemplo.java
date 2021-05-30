4414b1ab9387ea9d47d72eca10c1c37189c64bfb
WillianFarias <williansousafarias@gmail.com>
2021-05-30 20:41:03 -0300

    finalizacao releases java 10

--- advanced-development-in-java/src/aula05/InferenciaExemplo.java
+++ advanced-development-in-java/src/aula05/InferenciaExemplo.java
@@ -4,16 +4,26 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.net.URLConnection;
 import java.util.stream.Collectors;
 
 public class InferenciaExemplo {
 
 	public static void main(String[] args) throws IOException {
-		URL url = new URL("https://docs.oracle.com/javase/10/language/");
-		URLConnection urlConnection = url.openConnection();
-		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-		
-		System.out.println(bufferedReader.lines().collect(Collectors.joining()).replaceAll(">", ">\n"));
+
+	}
+
+	public static void connectAndPrintURLJavaOracle() throws IOException {
+		// var nao pode ser usado em nivel de classe
+		// var nao pode ser usado como parametro
+		// var nao pode ser usado em variaveis locais nao iniciadas
+
+		var url = new URL("https://docs.oracle.com/javase/10/language/");
+		var urlConnection = url.openConnection();
+
+		try (var bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()))) {
+			System.out.println(bufferedReader.lines().collect(Collectors.joining()).replaceAll(">", ">\n"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
